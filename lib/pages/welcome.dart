4c97b2192a1a7a80386758ffab3ba031d68b3f87
emon1432 <ovirajemon143@gmail.com>
2022-09-05 01:07:03 +0600

    add background image

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -6,7 +6,21 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: const Color(0xFFFFA600)),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          color: const Color(0xFFFFA600),
+          image: DecorationImage(
+            image: AssetImage('assets/images/welcome.jpg'),
+            // fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Text("Welcome"),
+          ],
+        ),
+      ),
     );
   }
 }
