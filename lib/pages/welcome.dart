938597a37f5a2fe3ad8c7fe1bb9984e2fd8a1646
emon1432 <ovirajemon143@gmail.com>
2022-09-08 00:51:14 +0600

    added button and welcome text

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -9,16 +10,51 @@ class WelcomeScreen extends StatelessWidget {
       body: Container(
         width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
-          color: const Color(0xFFFFA600),
+          color: Color(0xFFFFA600),
           image: DecorationImage(
             image: AssetImage('assets/images/welcome.jpg'),
             // fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          children: [
-            Text("Welcome"),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 100.0,
+          ),
+          child: Column(
+            children: [
+              Text(
+                "Welcome to \n Quiz App",
+                style: GoogleFonts.fredokaOne(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              //start button
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 130,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(60, 10, 60, 10),
+                    child: Text(
+                      "Start",
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    // backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
